f188111d1516f70ae33278af22ba81ccfa333dc1
Hoai Phuong <hoaiphuong260803@gmail.com>
2023-05-21 17:16:55 +0700

    ?

--- DoAnLTM/CoCaro/CreateARoom.cs
+++ DoAnLTM/CoCaro/CreateARoom.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,36 @@ namespace CoCaro
 {
     public partial class CreateARoom : Form
     {
+        private TcpListener listener;
+        private TcpClient client;
+        private NetworkStream stream;
         public CreateARoom()
         {
             InitializeComponent();
+        }
+
+        private void btn_CreateARoom_Click(object sender, EventArgs e)
+        {
+            //Mở form của người chơi đóng vai trò là chủ phòng
+            Room_SERVER newRoom = new Room_SERVER();
+            newRoom.Show();
+            this.Close();
+
+            //Tạo server mới
+            IPAddress ipAddress = IPAddress.Parse(txt_IPServer.Text);
+            int ServerPort = 3030;
+
+            //Khởi tạo lắng nghe kết nối từ Client
+            listener = new TcpListener(IPAddress.Any, ServerPort);
+            listener.Start();
+
+            //Bắt đầu chấp nhận kết nối từ Client
+            listener.BeginAcceptTcpClient(AcceptCallBack, null);
+        }
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            client = listener.EndAcceptTcpClient(ar);
+            stream = client.GetStream();
         }
     }
 }
