deb0f478ba9d942ff3cbf3ba176a15a354591de5
Hoai Phuong <hoaiphuong260803@gmail.com>
2023-05-23 11:25:08 +0700

    sua 1 chut

--- DoAnLTM/CoCaro/JoinARoom.cs
+++ DoAnLTM/CoCaro/JoinARoom.cs
@@ -12,9 +12,11 @@ namespace CoCaro
 {
     public partial class JoinARoom : Form
     {
+        List<Room> rooms;
         public JoinARoom()
         {
             InitializeComponent();
+            this.rooms = rooms;
         }
         private void btn_Join_Click(object sender, EventArgs e)
         {
